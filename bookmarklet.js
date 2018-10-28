e2b32e5b00ce84d7d3d6ee0906b6fcd4f901602d
Charles Stover <github@charlesstover.com>
2018-10-27 19:53:12 -0500

    scryfall support

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,14 +1,20 @@
 javascript:
-var cardImage = document.getElementById('ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cardImage');
-var cardTypes = document.getElementById('ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_typeRow').getElementsByClassName('value').item(0).innerText;
-var creature = cardTypes.match(/Creature/) ? true : false;
-var multiverseid = cardImage.getAttribute('src').match(/multiverseid=(\d+)/)[1];
-var box =
-  confirm('Hide copyright?') ?
-    'box=' + (creature ? 'creature' : '') + '&' :
-    '';
-cardImage.setAttribute(
-  'src',
-  'http://docker:13207/index.php?' + box + 'multiverseid='  + multiverseid
-);
+if (location.host === 'scryfall.com') {
+  var url = location.pathname.match(/\/card\/([^/]+\/[^/]+)\//)[1];
+  location.href = 'http://docker:13207/index.php?url=' + url;
+}
+else {
+  var cardImage = document.getElementById('ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cardImage');
+  var cardTypes = document.getElementById('ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_typeRow').getElementsByClassName('value').item(0).innerText;
+  var creature = cardTypes.match(/Creature/) ? true : false;
+  var multiverseid = cardImage.getAttribute('src').match(/multiverseid=(\d+)/)[1];
+  var box =
+    confirm('Hide copyright?') ?
+      'box=' + (creature ? 'creature' : '') + '&' :
+      '';
+  cardImage.setAttribute(
+    'src',
+    'http://docker:13207/index.php?' + box + 'multiverseid='  + multiverseid
+  );
+}
 void(0);
