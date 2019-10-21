80e405e78d4394001fa0114ea32a3a0d69e0afe5
Charles Stover <github@charlesstover.com>
2019-10-20 23:07:03 -0700

    extended border art

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# makeplayingcards
-Generates higher-resolution card images with padding.
+# MakePlayingCards
+
+Using Apache, PHP, the GD library, the Scryfall card image API, and Docker, the
+`mtgenius/makeplayingcards` Docker image generates higher resolution (816px wide
+by 1110px tall) _Magic: The Gathering_ card images with filled-in corners and
+extended borders.
+
+## Commands
+
+The following commands are shortcuts for quickly manipulating a Docker container
+named `makeplayingcards` on the `reverse-proxy` network.
+
+- `yarn build` builds the Docker image.
+- `yarn-build-run` builds the Docker image, stops and removes the existing
+  Docker container, and runs the new Docker container.
+- `yarn run` runs the Docker container.
+- `yarn rm` removes the existing Docker container.
+- `yarn stop` stops the existing Docker container.
--- src/makeplayingcards.php
+++ src/makeplayingcards.php
@@ -14,28 +14,52 @@ else if (array_key_exists('url', $_GET)) {
 }
 
 if (!$url) {
+  header('Content-Type: text/plain; charset=utf-8');
   exit('Cannot find URL.');
 }
+
+$stdout = fopen('php://stdout', 'w');
+fwrite($stdout, 'URL: ' . $url . PHP_EOL);
+fclose($stdout);
 
 $inner_height = 1040;
 $inner_width = 745;
 $outer_height = 1110;
 $outer_width = 816;
 
+$padding_left = round(($outer_width - $inner_width) / 2);
+$padding_top = round(($outer_height - $inner_height) / 2);
+
 $old_image_string = file_get_contents($url);
 
 $old_image = imagecreatefromstring($old_image_string);
 list($old_width, $old_height) = getimagesizefromstring($old_image_string);
 
-$border_color = imagecolorat($old_image, 0, 520);
-
 $new_image = imagecreatetruecolor($outer_width, $outer_height);
-imagefill($new_image, 0, 0, $border_color);
 
+// Paste the new image so that we can grab color from it.
 imagecopyresampled(
   $new_image, $old_image,
-  round(($outer_width - $inner_width) / 2),
-  round(($outer_height - $inner_height) / 2),
+  $padding_left, $padding_top,
+  0, 0,
+  $inner_width, $inner_height,
+  $old_width, $old_height
+);
+
+// Grab the border color from the bottom of the image.
+$border_color = imagecolorat(
+  $new_image,
+  round($outer_width / 2),
+  $outer_height - $padding_top - 1
+);
+
+// Fill the entire image with the border color.
+imagefill($new_image, 0, 0, $border_color);
+
+// Paste the new image on top of the border color.
+imagecopyresampled(
+  $new_image, $old_image,
+  $padding_left, $padding_top,
   0, 0,
   $inner_width, $inner_height,
   $old_width, $old_height
@@ -51,8 +75,8 @@ $box =
 if ($box !== null) {
   imagefilledrectangle(
     $new_image,
-    $outer_width - $box[0], $outer_height - $box[1],
-    $outer_width, $outer_height,
+    $outer_width - $padding_left - $box[0],       $outer_height - $box[1],
+    $outer_width - $padding_left, $outer_height - $padding_top,
     $border_color
   );
 }
@@ -60,29 +84,108 @@ if ($box !== null) {
 // Top Left Corner
 imagefilledrectangle(
   $new_image,
-  36, 35, 36 + 27, 35 + 26,
+  $padding_left,      $padding_top,
+  $padding_left + 25, $padding_top + 25,
+  $border_color
+);
+imagefilledrectangle(
+  $new_image,
+  $padding_left,      $padding_top,
+  $padding_left + 35, $padding_top + 10,
+  $border_color
+);
+imagefilledrectangle(
+  $new_image,
+  $padding_left,      $padding_top,
+  $padding_left + 10, $padding_top + 35,
   $border_color
 );
 
 // Top Right Corner
 imagefilledrectangle(
   $new_image,
-  755, 35, 755 + 26, 35 + 27,
+  $outer_width - $padding_left - 25, $padding_top,
+  $outer_width - $padding_left,      $padding_top + 25,
+  $border_color
+);
+imagefilledrectangle(
+  $new_image,
+  $outer_width - $padding_left - 35, $padding_top,
+  $outer_width - $padding_left,      $padding_top + 10,
+  $border_color
+);
+imagefilledrectangle(
+  $new_image,
+  $outer_width - $padding_left - 10, $padding_top,
+  $outer_width - $padding_left,      $padding_top + 35,
   $border_color
 );
 
 // Bottom Left Corner
 imagefilledrectangle(
   $new_image,
-  36, 1048, 36 + 26, 1075,
+  $padding_left,      $outer_height - $padding_left - 25,
+  $padding_left + 25, $outer_height - $padding_left,
+  $border_color
+);
+imagefilledrectangle(
+  $new_image,
+  $padding_left,      $outer_height - $padding_left - 35,
+  $padding_left + 10, $outer_height - $padding_left,
+  $border_color
+);
+imagefilledrectangle(
+  $new_image,
+  $padding_left,      $outer_height - $padding_left - 10,
+  $padding_left + 35, $outer_height - $padding_left,
   $border_color
 );
 
 // Bottom Right Corner
 imagefilledrectangle(
   $new_image,
-  755, 1048, 755 + 26, 1075,
+  $outer_width - $padding_left - 25, $outer_height - $padding_top - 25,
+  $outer_width - $padding_left,      $outer_height - $padding_top,
   $border_color
+);
+imagefilledrectangle(
+  $new_image,
+  $outer_width - $padding_left - 10, $outer_height - $padding_top - 35,
+  $outer_width - $padding_left,      $outer_height - $padding_top,
+  $border_color
+);
+imagefilledrectangle(
+  $new_image,
+  $outer_width - $padding_left - 35, $outer_height - $padding_top - 10,
+  $outer_width - $padding_left,      $outer_height - $padding_top,
+  $border_color
+);
+
+// Full art top
+imagecopyresampled(
+  $new_image, $new_image,
+  $padding_left, 0,
+  $padding_left, $padding_top,
+  $inner_width, $padding_top,
+  $inner_width, 1
+);
+
+// Full art left
+imagecopyresampled(
+  $new_image, $new_image,
+  0, $padding_top,
+  $padding_left, $padding_top,
+  $padding_left, $inner_height,
+  1, $inner_height
+);
+
+// Full art right
+imagecopyresampled(
+  $new_image, $new_image,
+  $outer_width - $padding_left, $padding_top,
+  $outer_width - $padding_left, $padding_top,
+  $padding_left, $inner_height,
+  1, $inner_height
 );
 
 header('Content-Type: image/png; charset=utf-8');
